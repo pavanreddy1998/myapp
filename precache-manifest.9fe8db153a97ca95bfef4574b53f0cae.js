9adc97753c885acaf053ac2b24a7f6ad6d218a45
pavanreddy21 <pavanreddygottimukkula@gmail.com>
2020-08-09 15:26:37 +0530

    Updates

---
+++ precache-manifest.9fe8db153a97ca95bfef4574b53f0cae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d377d0c327120d624ac1512db342b68",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5dad2cdc42c3f55a2ca8",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "205a588803857b80ec54",
+    "url": "/my-app/static/js/2.b8fb400c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-app/static/js/2.b8fb400c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dad2cdc42c3f55a2ca8",
+    "url": "/my-app/static/js/main.0921ffd9.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
